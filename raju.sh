330feec3d200a51edadcf175e8f7301917e84078
varanasiraju <46042371+varanasiraju@users.noreply.github.com>
2019-11-21 16:15:15 +0530

    Create raju.sh

---
+++ raju.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+echo "WELCOME TO EAP PROJECT TEAM"  
+echo "Kindly find the Versions of JENKINS JAVA and GIT"
+java -jar /usr/share/jenkins/jenkins.war --version
+java -version
+git --version
+echo "kindly find jenkins server configurations"
+uname -ar
